28414c5b5d46a6ad581fda97009921f3e5ecf9f8
Aryan <dev.404.me@gmail.com>
2025-01-01 09:57:51 +0530

    second

--- app/new/page.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {};
-const newPage = (props: Props) => {
-  return (
-    <div data-box className="h-[100vh] w-full flex items-center justify-center">
-      <div className="h-96 w-96 bg-red-700"></div>
-    </div>
-  );
-};
-export default newPage;
